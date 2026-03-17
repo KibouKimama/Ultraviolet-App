fc351e5a7706e1b227bc01786308d917ace422d8
KibouKimama <kiboukimama@gmail.com>
2026-03-17 18:50:55 +0900

    Fix quotes in UV config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
 	prefix: "/service/",
-	bare: 'https://odd-hall-1477.kiboukimama.workers.dev/bare/',
+	bare: "https://odd-hall-1477.kiboukimama.workers.dev/bare/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/uv/uv.handler.js",
