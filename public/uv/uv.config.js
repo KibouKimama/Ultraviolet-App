2ff207f56f6e815cee18b150fec47af32d6d61ba
KibouKimama <kiboukimama@gmail.com>
2026-03-17 18:42:30 +0900

    Change UV service prefix in config

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-	prefix: "/uv/service/",
+	prefix: "/service/",
 	bare: 'https://odd-hall-1477.kiboukimama.workers.dev/bare/',
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
